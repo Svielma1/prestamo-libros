9ee7a4be89f86a0600bd1a00054254376bcfee05
yskaaren <karengutierrez0414@gmail.com>
2024-06-25 09:01:06 -0400

    posicion del footer y logica

--- ApiMongo/db.js
+++ ApiMongo/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const mongoURI = 'mongodb+srv://sebastianvielma03:Inacap123@serverinacap.aw3r9qk.mongodb.net/bibliotecap?retryWrites=true&w=majority&appName=ServerInacap'
 
-mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(mongoURI)
 .then(() => console.log('Conectado a la base de datos.'))
 .catch(err => console.log(err))
 
--- Flask/app.py
+++ Flask/app.py
@@ -6,33 +6,39 @@ app = Flask(__name__,
             static_folder='static',
             template_folder='templates')
 
-username = "admin"
-password = "admin123"
+username_default = "admin"
+password_default = "admin123"
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('login'))
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
-    if username == username and password == password:
-        print(jsonify({'success': True}))
-        return redirect(url_for('prestamos'))
-    else:
-        print(jsonify({'success': False, 'message': 'Invalid credentials'}))
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == username_default and password == password_default:
+            return redirect(url_for('prestamos'))
+        return render_template('index.html')
+    
 
-@app.route('/prestamos')
+
+@app.route('/prestamos', methods=['GET'])
 def prestamos():
-    datos = requests.get('http://127.0.0.1:2000/prestamos')
-    if datos.status_code == 200:
-        prestamos = datos.json()
-        return render_template('prestamos.html', prestamos=prestamos)
-    else:
-        return jsonify({'error':'Error al obtener los datos desde la ruta de la api Node.js'})
+    try:
+        datos = requests.get('http://127.0.0.1:2000/prestamos')
+        if datos.status_code == 200:
+            prestamos = datos.json()
+            return render_template('prestamos.html', prestamos=prestamos)
+        return render_template('prestamos.html', error='No hay prestamos aún')
+    except Exception as e:
+        return render_template('prestamos.html', error='Error al obtener prestamos')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
